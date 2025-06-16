36a13eb9c6cc2bfebcad548eda963dbff8387bd8
ahlaiptek <iptekahla@gmail.com>
2025-06-17 05:24:29 +0700

    Add Header component and integrate it into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import "./index.css";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Dashboard/>
+    <>
+      <Header />
+      <Dashboard />
+    </>
   );
 }
 
